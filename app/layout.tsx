2b59261027517ff869a3c3a4fc90186636df25de
mmminggu <mnyu1017@gmail.com>
2024-04-23 15:56:46 +0900

    nextjs 홈페이지 구축

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,17 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
+import { Inter as FontSans } from "next/font/google"
+import { cn } from "@/lib/utils"
+import {ApolloWrapper} from "@/app/ApolloWrapper"
 
 const inter = Inter({ subsets: ["latin"] });
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +25,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+          className={cn(
+            "min-h-screen bg-background font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+        <ApolloWrapper>
+      <header className="flex justify-between p-4">
+        <Link href="/">home</Link>
+        <Link href="/cart">cart</Link>
+      </header>
+      {children}
+    </ApolloWrapper>
+      </body>
     </html>
   );
 }
